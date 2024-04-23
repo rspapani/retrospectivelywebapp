48eb9d3e5f729b08fdc30f8c8a26911ef48e2ce5
Rishi Papani <rspapani@outlook.com>
2024-04-23 08:45:45 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
     <div className="container">
       <div className="gif-background"></div> {/* Added GIF background container */}
       <header className="header">
-        <div className="brand">Retrospectively</div>
+        <div className="brand">
+          <a href="/" className="menu-link">
+            Retrospectively
+          </a>
+        </div>
         <nav className="menu">
           <a href="/about" className="menu-link">About</a> {/* Updated to be a link */}
         </nav>
@@ -30,7 +34,7 @@ function App() {
             </div>
 
             <label htmlFor="comment">How do you want to improve your conversation? </label>
-            <textarea id="comment" name="comment" placeholder="Your Answer (optional)" rows="4"></textarea>
+            <textarea id="comment" name="comment" placeholder="Your Answer (optional)" rows="3"></textarea>
 
             <button type="submit">Submit</button>
           </form>
